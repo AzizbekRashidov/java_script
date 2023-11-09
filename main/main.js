bb06fcbe0f5f67c7e3d62df09412f7e5c12db8e1
AzizbekRashidov <azizbekrashidovuz@gmail.com>
2023-11-09 17:49:57 +0500

    java script

---
+++ main/main.js
@@ -0,0 +1,36 @@
+var Name = String(prompt("Ismingiz:"));
+console.log(Name);
+
+var Surname = String(prompt("Familiyangiz:"));
+console.log(Surname);
+
+var Age = Number(prompt("Yoshingiz:"));
+console.log(Age);
+
+var Phone = Number(prompt("Telefon raqamingiz:"));
+console.log(Phone);
+
+var Job = String(prompt("Kasbingiz:"));
+console.log(Job);
+
+var Sport = String(prompt("Sevimli sportingiz:"));
+console.log(Sport);
+
+var Music = String(prompt("Sevimli musiqangiz:"));
+console.log(Music);
+
+var Address = String(prompt("Manzilingiz:"));
+console.log(Address);
+
+var Answers = [
+    "Name:"+""+Name+"\n"+
+    "Surname:"+""+Surname+"\n"+
+    "Age:"+""+Age+"\n"+
+    "Phone:"+""+Phone+"\n"+
+    "Job:"+""+Job+"\n"+
+    "Sport:"+""+Sport+"\n"+
+    "Music:"+""+Music+"\n"+
+    "Address:"+""+Address+"\n"
+];
+
+alert(Answers);
